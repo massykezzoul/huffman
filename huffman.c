73138cad28845993b1427c4acfde03c1f1bf7081
Massili Kezzoul <massy.kezzoul@gmail.com>
2018-12-08 13:51:29 +0100

    nouvelle version du compresseur

--- huffman.c
+++ huffman.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "arbre/arbre.h"
 #include "proba/proba.h"
 #include "compress/compress.h"
+
+void affiche_info(FILE* source, FILE* dest) {
+	/*
+		affiche les info suivants :			
+			taille originelle de la source, 
+			taille compressée et gain en pourcentage,
+	*/
+
+	long int taille_source = 0;
+	long int taille_dest = 0;
+
+	/* Taille originelle de la source */
+	fseek(source,0,SEEK_END);
+	
+	/* Taille compressée et gain en pourcentage */
+	fseek(dest,0,SEEK_END);
+
+	taille_source = ftell(source);
+	taille_dest = ftell(dest);
+
+	printf("Information de compression : \n");
+	printf("\tTaille Originelle de la source : %ld Octet\n",taille_source);
+	printf("\tTaille compressée : %ld Octet\n",taille_dest);	
+	printf("\tGain en pourcentage : %0.2f%%\n",(1 - ((float)taille_dest / taille_source)) * 100);
+
+}
 
 
 int main(int argc, char const *argv[])
@@ -13,88 +40,95 @@ int main(int argc, char const *argv[])
 	/* Declaration des variables */
 	FILE* fichier;
 	FILE* compress;
-	unsigned long int taille_d,taille_arbre,i;
+	unsigned long int taille_f,taille_d,taille_arbre,i;
 	unsigned long int compte[256];
 	float proba[256];
-
 
 	distribution* d;
 	
 	node* arbre;
 
 	codage* tab_code;
+	codage* tab_code2;
 	/* initialisation des variables */
-
-	fichier = fopen(argv[1],"r");
-
-	taille_d = 0;
-	taille_arbre = 0;
-	for (i = 0; i < 256; ++i)
-		compte[i] = 0;
-	for (i = 0; i < 256; ++i)
-		proba[i] = 0.0;	
-
-	/* Lecture des occurences et des probabilité à partir du fichier */
-
-	if (fichier == NULL) {
-		printf("Erreur lors de la lecture du fichier '%s', merci de verifier son existance\n",argv[1]);
-		exit(1);
+	if (argc > 2) {
+		fichier = fopen(argv[1],"r");
+		compress = fopen(argv[2],"w");
 	} else {
-		/* Calcule des occurences */
-		taille_d = calcul_occurences(compte,fichier);
-		/* Calcule des probabilités */
-		calcul_proba(proba,compte,taille_d,fichier);
-
-		fclose(fichier);
+		printf("Manque un argument.\n");
+		exit(1);
 	}
 
-	/* Nombre de char different */
-	taille_d = nb_char(proba);
+	/* -------------- COMPRESSION ------------------------------------------------------------------------ */
 
-	/* declaration du tableau de la destribution */ 		
-	d = calloc(taille_d,sizeof(distribution));
+		taille_d = 0;
+		taille_arbre = 0;
+		for (i = 0; i < 256; ++i)
+			compte[i] = 0;
+		for (i = 0; i < 256; ++i)
+			proba[i] = 0.0;	
 
-	/* insertion des probabilités (trié en Ascendant) ps:pourquoi trié? je ne sais pas*/
-	init_distribution(proba,d,taille_d);
+		/* Lecture des occurences et des probabilité à partir du fichier */
 
-	/* Affiche la distribution 	*/ 
-	/* affiche_d(d,taille_d); 	*/
+		if (fichier == NULL || compress == NULL) {
+			printf("Erreur lors de la lecture du fichier '%s', merci de verifier son existance\n",argv[1]);
+			exit(1);
+		} else {
+			/* Calcule des occurences */
+			taille_f = calcul_occurences(compte,fichier);
+			/* Calcule des probabilités */
+			calcul_proba(proba,compte,taille_f);
 
+		}
 
-	/* ----------------------------------------------------------------------------------------------------*/
-	/* Etape de creation de l'arbre de huffman */
+		/* Nombre de char different */
+		taille_d = nb_char(proba);
 
-	/* Construction de l'arbre */
-	arbre = make_arbre(d,taille_d,&taille_arbre);
+		/* Ecriture de l'entête du fichier */
+		ecrire_entete(compress,compte,taille_d);
 
-	/*affiche_arbre(arbre,taille_arbre);		*/
-	
-	/* Determiner le codage de chaque char sous forme d'un tableau	*/
-	tab_code = calcul_codage(arbre,taille_d);
+		/* declaration du tableau de la destribution */ 		
+		d = calloc(taille_d,sizeof(distribution));
 
-	/* Affichage de la table de huffman associé */
-	/*affiche_codage(tab_code,taille_d);		*/
+		/* insertion des probabilités */
+		init_distribution(proba,d,taille_d);
 
-	/* ----------------------------------------------------------------------------------------------------*/
-	/* Etape de creation du fichier compressé */
+		/* Affiche la distribution 	*/ 
+		affiche_d(d,taille_d);
+		
+		/* ----------------------------------------------------------------------------------------------------*/
+		/* Etape de creation de l'arbre de huffman */
+		
+		/* Construction de l'arbre */
+		arbre = make_arbre(d,taille_d,&taille_arbre);
+		affiche_arbre(arbre,taille_arbre);
+		
+		/* Determiner le codage de chaque char sous forme d'un tableau	*/
+		tab_code = calcul_codage(arbre,taille_d);
+		
+		/* Affichage de la table de huffman associé */
+		affiche_codage(tab_code,taille_d);
+		
+		/* ----------------------------------------------------------------------------------------------------*/
+		/* Etape de creation du fichier compressé */		
 
-
-	if (argv[2] == NULL)
-		compress = fopen("compressed.huf","w");
-	else 
-		compress = fopen(argv[2],"w");
-
-	fichier = fopen(argv[1],"r");
-
-	if ((compress == NULL) || (fichier == NULL)) {
-		printf("Erreur à la creation du fichier, ou la réouverture du fichier .\n");
-		exit(1);
-	} else {
 		/* ecriture du fichier compressé */
-		compresse(fichier,compress,tab_code);
+		if (fichier==NULL || compress == NULL) {
+			printf("Erreur!!\n");
+			exit(1);
+		}
+
+		compresse(fichier,compress,tab_code,taille_d);
+
+		printf("Fichier Compressé Créer avec Succée.\n");
+
+		affiche_info(fichier,compress);
+		
 		fclose(fichier);
 		fclose(compress);
-	}
+		
+
+	/* -------------- FIN COMPRESSION ------------------------------------------------------------------------ */
 
 
 
